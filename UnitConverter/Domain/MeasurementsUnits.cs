46fe5b8b78908820d9d89816fdfbe920e0ecc7b3
julianastc <julianasantostav@gmail.com>
2025-01-30 14:07:12 -0300

    create enum MeasurementsUnits

---
+++ UnitConverter/Domain/MeasurementsUnits.cs
@@ -0,0 +1,11 @@
+namespace UnitConverter.Domain;
+
+public enum MeasurementsUnits
+{
+    Meter = 1,
+    Kilometer = 2,
+    Centimeter = 3,
+    Millimeter = 4,
+    Decimeter = 5,
+    Decameter = 6
+}
